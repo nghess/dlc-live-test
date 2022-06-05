e95f4876eddb0359b77c0869f5fdc4514fac7073
Nate Gonzales Hess <nhess@uoregon.edu>
2022-06-05 00:25:38 -0700

    camera working

--- test-exercise.py
+++ test-exercise.py
@@ -1,17 +1,20 @@
 from dlclive import DLCLive, Processor
 from PIL import Image
 from numpy import asarray
+import time
 
-img = Image.open('frames/mouse.tif')
-
-data = asarray(img)
 folder = 'model/'
-
-print(data.ndim)
-
 dlc_proc = Processor()
 dlc_live = DLCLive(folder, processor=dlc_proc)
-dlc_live.init_inference(data)
-pose = dlc_live.get_pose(data)
 
-print(pose)
+for i in range(0, 10):
+
+    tic = time.perf_counter()
+    img = Image.open('frames/mouse.tif')
+    data = asarray(img)
+    dlc_live.init_inference(data)
+    pose = dlc_live.get_pose(data)
+    toc = time.perf_counter()
+
+    print(pose)
+    print(f"{tic-toc:0.4f}")
