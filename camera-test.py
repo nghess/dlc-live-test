8dad4b7003f0a785991016b179a9921354877737
Nate Gonzales Hess <nhess@uoregon.edu>
2022-06-05 01:13:41 -0700

    camera working

--- camera-test.py
+++ camera-test.py
@@ -1,16 +1,18 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
+i = 0
 
 # Check if the webcam is opened correctly
 if not cap.isOpened():
     raise IOError("Cannot open webcam")
 
 while True:
+    i += 1
     ret, frame = cap.read()
     frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
     cv2.imshow('Input', frame)
-    print(frame)
+    cv2.imwrite("output/" + str(i) + ".png", frame)
     c = cv2.waitKey(1)
     if c == 27:
         break
