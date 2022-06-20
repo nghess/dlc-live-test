ceeafc690f497d8aef0551a08797a49296cd0904
Nate Gonzales Hess <nhess@uoregon.edu>
2022-06-20 14:27:51 -0700

    head angle test

--- head-angle.py
+++ head-angle.py
@@ -9,13 +9,6 @@ dlc_live = DLCLive(folder, processor=dlc_proc)
 dlc_live.init_inference()
 
 i = 0
-radius = 20
-thickness = -1
-flow = 0
-
-def gaussian(x, mu, sig):
-    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
-
 
 while True:
     # Load frame
@@ -28,22 +21,10 @@ while True:
     nose = (int(pose[0, 0]), int(pose[0, 1]))
     head = (int(pose[1, 0]), int(pose[1, 1]))
     body = (int(pose[2, 0]), int(pose[2, 1]))
-    if nose[0] > head[0]:
-        tilt = cv2.norm(head, nose)
-        flow = (nose[0]-head[0])/tilt
-    else:
-        flow = 0
 
-    #wind = nose[0]
-    # Calculate distance and activation
-    target = (int(frame.shape[1]-20), int(frame.shape[0]/2))
-    length = cv2.norm(target, nose)
-    laser = gaussian(length, 0, int(frame.shape[1]*.35))  # Collapse distance and run through 1d Gaussian
-
-    # Draw lines on Stage
+    # Draw lines on Stage for angle measurement
     stage = np.zeros((frame.shape[0], frame.shape[1]), np.uint8)  # Clear frame
     stage = cv2.line(stage, nose, head, 255, 1)
-    #stage = cv2.line(stage, (head[0], frame.shape[0]), (head[0], 0), 255, 1)
 
     # Perform Hough Transformation to detect lines
     hspace, angles, distances = hough_line(stage)
@@ -56,15 +37,26 @@ while True:
     # Obtain angle for each line
     angles = [a*180/np.pi for a in angle]
 
-    # Compute difference between the two lines
-    print(angles[0])
+    # Get length of radius for angle visualization
+    radius = cv2.norm(head, nose)
+    axes = (int(radius), int(radius))
 
     # Draw lines
     frame = cv2.line(frame, nose, head, (255, 255, 0), 2)
     frame = cv2.line(frame, (head[0], frame.shape[0]), (head[0], 0), (0, 0, 0), 1)
-    frame = cv2.putText(frame, str(int(angles[0])), (head[0]-25, head[1]-25), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255))
+    frame = cv2.putText(frame, str(int(angles[0])), (head[0]-50, head[1]-50), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255))
 
-
+    # Conditionals to draw the radius on correct quadrant
+    if nose[0] > head[0]:
+        if angles[0] > 0:
+            frame = cv2.ellipse(frame, head, axes, -90, angles[0], 0, (0, 0, 255))
+        else:
+            frame = cv2.ellipse(frame, head, axes, 90, angles[0], 0, (0, 0, 255))
+    else:
+        if angles[0] > 0:
+            frame = cv2.ellipse(frame, head, axes, 90, angles[0], 0, (0, 0, 255))
+        else:
+            frame = cv2.ellipse(frame, head, axes, -90, angles[0], 0, (0, 0, 255))
 
     # Show video
     cv2.imshow('Pose', frame)
