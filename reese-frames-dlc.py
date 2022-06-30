8f80d055e637e66e5296b56d2f6f4bc1ece8a8e8
nghess <nhess@uoregon.edu>
2022-06-29 17:03:57 -0700

    FPS counter

--- reese-frames-dlc.py
+++ reese-frames-dlc.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 import numpy as np
 from dlclive import DLCLive, Processor
 
@@ -11,16 +12,18 @@ i = 0
 radius = 20
 thickness = -1
 flow = 0
+fps = 1
 
 def gaussian(x, mu, sig):
     return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
 
 
 while True:
+    start_time = time.time() # start time of the loop
     # Load frame
     i += 1
     frame = cv2.imread('frames/ ('+str(i)+').jpg')
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    #frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
 
     # Get poses
     pose = dlc_live.get_pose(frame)
@@ -46,7 +49,8 @@ while True:
     #frame = cv2.circle(frame, nose, int(radius*laser), (int(255*laser), int(255*laser), 255), thickness)
     frame = cv2.circle(frame, nose, int(radius*flow*laser), (int(255*flow), int(255*flow), 0), thickness)
 
-
+    fps = f"FPS: {round(1.0 / (time.time() - start_time))}"
+    frame = cv2.putText(frame, fps, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255))
 
     # Show video
     cv2.imshow('Pose', frame)
