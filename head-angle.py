25651fd9595f6a755598f8878c4e6f7ed5e16690
Nate Gonzales Hess <nhess@uoregon.edu>
2022-08-30 20:55:46 -0700

    nose dip

--- head-angle.py
+++ head-angle.py
@@ -11,6 +11,15 @@ dlc_live.init_inference()
 i = 0
 line_length = []
 
+
+def orbit(point1, point0, degrees):
+    deg = degrees*(np.pi/180)
+    c = (point1[0]-point0[0], point1[1] - point0[1])
+    c = (int(c[0]*np.cos(deg) - c[1]*np.sin(deg)), int(c[0]*np.sin(deg) + c[1]*np.cos(deg)))
+    c = (c[0]+point0[0], c[1]+point0[1])
+    return c
+
+
 while True:
     # Load frame
     i += 1
@@ -51,10 +60,9 @@ while True:
     elif nose[0] < head[0] and degree > 0:
         degree = 180 + degree
 
-    # Draw lines
-    #line_length.append(cv2.norm(nose, head))
-    line_dip = cv2.norm(nose, head)/48.5
-    line_color = (255*line_dip, 255*line_dip, (-255*line_dip)+255)
+
+    # Draw Annotations
+    line_color = (255, 255, 0)
     frame = cv2.line(frame, nose, head, line_color, 1, lineType=cv2.LINE_AA)
     frame = cv2.line(frame, (head[0], int(head[1]-radius)), head, line_color, 1, lineType=cv2.LINE_AA)
     frame = cv2.putText(frame, str(degree), (head[0]-50, head[1]-50), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255), lineType=cv2.LINE_AA)
@@ -65,6 +73,22 @@ while True:
     else:
         frame = cv2.ellipse(frame, head, axes, -90, 0, degree, line_color, lineType=cv2.LINE_AA)
 
+    # Draw nose depth
+    line_dip = cv2.norm(nose, head)/48.5  # 48.5 is the distance between the nose and head in pixels
+    nose_color = (255*line_dip, 255*line_dip, (-255*line_dip)+255)
+    nose_depth = np.sqrt(1-line_dip**2)  # Calculate Z dip
+    nose_dip_theta = np.degrees(np.arcsin(nose_depth))  # Calculate theta of dip
+
+    # Draw nose dip schematic
+    n_1 = (20, 50)
+    n_0 = (60, 50)
+    n_schema = orbit(n_0, n_1, nose_dip_theta)
+    print(n_schema)
+    frame = cv2.circle(frame, nose, 3, nose_color, -1, lineType=cv2.LINE_AA)
+    frame = cv2.line(frame, n_1, (int(n_schema[0]), int(n_schema[1])), nose_color, 2, lineType=cv2.LINE_AA)
+
+
+
     # Show video
     cv2.imshow('Pose', frame)
     cv2.imwrite("output/head_angle/" + str(i) + ".png", frame)
@@ -72,7 +96,5 @@ while True:
 
     # Reset loop
     if i == 969:
-        #i = 0
-
-        print(max(line_length))
-        break
+        i = 0
+        #break
